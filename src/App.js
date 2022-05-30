ff775351bb07cb11b86c1c81502dc58536e61924
em-cotter <ecotter22@gmail.com>
2022-05-30 09:19:39 +0200

    adding bootstrap and a grid

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 
 import './App.css';
 import Page from './Page';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div className="App">
+      <div classNAme="Container">
       <header className="App-header"></header>
       <Page />
+      </div>
     </div>
   );
 }
--- src/Page.js
+++ src/Page.js
@@ -1,5 +1,6 @@
 import React from "react";
 import './Page.css'
+
 export default function Page (){
     return (
       <div>
@@ -10,20 +11,41 @@ export default function Page (){
           Hi, nice to meet you. These are the projects I have worked on in the
           past...
         </p>
-        <h2>Weather App </h2>
-        <span>coded in ReactJS</span>
-        <iframe
-          src="https://zingy-concha-edec1d.netlify.app/"
-          height="400"
-          width="500"
-        ></iframe>
-        <h2>Dictionary</h2>
-        <span>coded in ReactJS</span>
-        <iframe
-          src="https://stunning-nougat-5bb688.netlify.app/"
-          height="400"
-          width="500"
-        ></iframe>
+        <div className="row">
+          <div className="col">
+            <iframe
+              className="Weather"
+              src="https://zingy-concha-edec1d.netlify.app/"
+              height="500"
+              width="500"
+              frameBorder="0"
+            >
+              Sorry
+            </iframe>
+          </div>
+          <div className="col WeatherInfo">
+            <h2>Weather App </h2>
+            <span>coded in ReactJS</span>
+          </div>
+        </div>
+       
+        <div className="row ">
+          <div className="col DictionaryInfo">
+            <h2>Dictionary</h2>
+            <span>coded in ReactJS</span>
+          </div>
+          <div className="col">
+            <iframe
+              className="Dictionary"
+              src="https://stunning-nougat-5bb688.netlify.app/"
+              height="500"
+              width="500"
+              frameBorder="0"
+            >
+              Sorry
+            </iframe>
+          </div>
+        </div>
         <div className="ContactMe">
           <span className="Contact">Contact: </span>
           <span>ecotter22@gmail.com</span>
